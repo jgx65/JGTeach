3e5cb7edd4b749a20d7d7d88d87f781b9daa68ed
Jerome Goudet <jgx65@users.noreply.github.com>
2022-08-01 21:51:30 +0200

    gametes of the founders can be entered as a 2n.founders*nl array

--- R/ibd2dos.R
+++ R/ibd2dos.R
@@ -2,19 +2,27 @@
 #'
 #' @description Given gametes, the output of \code{\link{drop.along.ped}} on a pedigree and
 #' and n.founders, the number of founders in the pedigree,
-#' return a matrix of dosages after having mapped at random
+#' return a matrix ni x nl of dosages after having mapped
 #' the 2n.founders alleles at each locus to 0 or 1.
-#' The probability of a 1 (the founders frequency of 1) is drawn from a
-#' \code{\link{beta}} distribution with parameters b1 and b2.
+#' The gametes of the founders are given in g.founders or
+#' frequencies at each locus are drawn from a \code{\link{beta}}
+#' distribution with parameters b1 and b2.
 #'
-#' @usage ibd2dos(gametes,n.founders,b1=1,b2=10)
+#' @usage ibd2dos(gametes,n.founders,g.founders=NULL,b1=1,b2=10)
 #'
 #' @param gametes a nixnlx2 array of gametes created by \code{\link{drop.along.ped}}
 #' @param n.founders the number of founders in the pedigree
+#' @param g.founders a (2*ni)xnl array with the bi-allelic gametes of the founders (default to NULL)
 #' @param b1 the 1st parameter of the \code{\link{beta}} distribution
-#' @param b2 the second parameter of the \code{\link{beta}} distribution
+#' @param b2 the 2nd parameter of the \code{\link{beta}} distribution
 #'
 #' @return a dosage matrix with ni rows and nl columns
+#'
+#' @details The gametes of  the founders can be entered as an (2*ni) x nl array. This insures
+#' any haplotype structure in the founders can be kept.  If g.founders is NULL, allelic frequencies
+#' in the founders are drawn from a beta distribution with parameters b1 (default=1)
+#' and b2 (default=10).
+#'
 #'
 #' @author Jerome Goudet \email{jerome.goudet@@unil.ch}
 #'
@@ -27,16 +35,24 @@
 #'
 #' @export
 #'
-ibd2dos<-function(gametes,n.founders,b1=1,b2=10){
+ibd2dos<-function(gametes,n.founders,g.founders=NULL,b1=1,b2=10){
+  nf2<-2*n.founders
+  nl<-dim(gametes)[2]
   tmp<-dim(gametes)[3]
   if ((is.na(tmp)) | (tmp!=2))
     stop("Error, gametes must be an array nixnlx2 \n as produced by the drop.along.ped function. Exiting")
   if((n.founders <1) | (n.founders >dim(gametes)[1]))
     stop("Error, n.founders must be the number of founders in the pedigree \n used to generate gametes. Exiting")
-  nf2<-2*n.founders
-  for (il in 1:dim(gametes)[[2]]){
-    nA<-rbeta(1,b1,b2)*nf2
-    alt<-sample(nf2,size=round(nA,digits=0))
+  if((!is.null(g.founders)) & (dim(g.founders)[1]!=nf2) & (dim(g.founders)[2]!=nl))
+    stop("Error, dim of g.founders must match be 2n.founders x nl. Exiting")
+
+  if (is.null(g.founders)) nA<-round(rbeta(nl,b1,b2)*nf2,digits=0)
+
+  for (il in 1:nl){
+    if (!is.null(g.founders))
+       alt<-which(g.founders[,il]==1)
+    else
+       alt<-sample(nf2,size=nA[il])
     x<-gametes[,il,] %in% alt
     gametes[,il,][x]<-1
     gametes[,il,][!x]<-0
